97265a4592d58f0008ab8ab0225a2a689531a796
Michael Felt <aixools@felt.demon.nl>
2026-03-19 16:16:01 +0000

    Keep state migration fully squashed: fold AccessRuleSyncAudit action help_text into 0000

--- bg/state/migrations/0000_initial.py
+++ bg/state/migrations/0000_initial.py
@@ -38,7 +38,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('request_id', models.CharField(blank=True, default='', max_length=64)),
                 ('requested_by', models.CharField(blank=True, default='', max_length=255)),
-                ('action', models.CharField(default='sync', max_length=16)),
+                ('action', models.CharField(default='sync', help_text='Operation that produced this record', max_length=16)),
                 ('state_before', models.JSONField(blank=True, default=dict)),
                 ('state_after', models.JSONField(blank=True, default=dict)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
